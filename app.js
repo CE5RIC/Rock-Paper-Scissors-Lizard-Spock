edde7868f67331a629fc298dd7e160a3724c86bd
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-19 11:12:16 +0200

    created click event for user and computer

--- app.js
+++ app.js
@@ -1,72 +1,26 @@
-function computerPlay(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
+    const options = document.querySelectorAll(".options");
 
-const computerSelection = computerPlay(['rock', 'paper', 'scissors', 'lizard', 'spock']);
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        const pChoice = this.textContent;
 
-console.log(computerSelection);
+        const cOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+        const cInput = cOptions[Math.floor(Math.random() * 5)];
 
-
-let choice = ('rock', 'paper', 'scissors', 'lizard', 'spock');
-
-const playerSelection = choice;
-
-console.log(playerSelection);
+        compareInputs(pChoice, cInput);
+      });
+    });
 
 
 
 
-function playRound(playerSelection, computerSelection) {
 
-    if (computerSelection == playerSelection) {
-        return 'Draw!';
-    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
-        return 'You Lose! Paper beats rock';
-    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
-        return 'You win! Paper beats rock';
-    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
-        return 'You lose! Scissors beats paper';
-    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
-        return 'You win! Scissors beats paper';
-    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
-        return 'You lose! Rock beats scissors';
-    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
-        return 'You win! Rock beats scissors';
-    } else if (computerSelection == 'lizard' && playerSelection == 'spock') {
-        return 'You lose! Lizard poisons Spock';
-    } else if (computerSelection == 'spock' && playerSelection == 'lizard') {
-        return 'You win! Lizard poisons Spock';
-    } else if (computerSelection == 'lizard' && playerSelection == 'rock') {
-        return 'You win! Rock crushes Lizard';
-    } else if (computerSelection == 'rock' && playerSelection == 'lizard') {
-        return 'You lose! Rock crushes Lizard';
-   }  else if (computerSelection == 'lizard' && playerSelection == 'scissors') {
-     return 'You win! Scissors decapitate Lizard';
-    } else if (computerSelection == 'scissors' && playerSelection == 'lizard') {
-     return 'You lose! Scissors decapitate Lizard';
-    } else if (computerSelection == 'lizard' && playerSelection == 'paper') {
-        return 'You lose! Lizard eats paper';
+
+
+    function compareInputs(pChoice, cInput) {
+      
+
+      // Tie check
+      if (pChoice === cInput) {
         
-    } else if (computerSelection == 'paper' && playerSelection == 'lizard') {
-        return 'You win! Lizard eats paper';
-    } else if (computerSelection == 'spock' && playerSelection == 'rock') {
-        return 'You win! Rock crushes spock';
-    } else if (computerSelection == 'rock' && playerSelection == 'spock') {
-        return 'You lose! Rock crushes spock';
-    } else if (computerSelection == 'spock' && playerSelection == 'scissors') {
-        return 'You lose! Spock smashes scissors';
-    } else if (computerSelection == 'scissors' && playerSelection == 'spock') {
-        return 'You win! Spock smashes scissors';
-    } else if (computerSelection == 'spock' && playerSelection == 'rock') {
-        return 'You lose! Spock vaporizes rock';
-    } else if (computerSelection == 'rock' && playerSelection == 'spock') {
-        return 'You win! Spock vaporizes Rock';
-    } else if (computerSelection == 'paper' && playerSelection == 'spock') {
-        return 'You lose! Paper disproves Spock';
-    } else if (computerSelection == 'spock' && playerSelection == 'paper') {
-        return 'You win! Paper disproves Spock';
-    }
-     }
-
-console.log(playRound(playerSelection, computerSelection));
-
+      }
