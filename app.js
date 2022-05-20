c783b66c848115f61983823b699588c1b8c02fe0
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-20 10:25:54 +0200

    fixed my reset function now working properly

--- app.js
+++ app.js
@@ -1,131 +1,118 @@
     
     
    
+   const options = document.querySelectorAll(".options");
 
-    
-    const options = document.querySelectorAll(".options");
-
-    let playerScore = 0;
-    let computerScore = 0;
+   let playerScore = 0;
+   let computerScore = 0;
 
 
-    console.log(options);
+   console.log(options);
 
-    options.forEach((option) => {
-      option.addEventListener("click", function () {
-        
-        const pChoice = this.textContent;
+   options.forEach((option) => {
+     option.addEventListener("click", function () {
+       
+       const pChoice = this.textContent;
 
-        const cOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-        const cInput = cOptions[Math.floor(Math.random() * 5)];
+       const cOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+       const cInput = cOptions[Math.floor(Math.random() * 5)];
 
 
-        
+       
 
 if (pChoice == cInput) {
-            playerScore++;
-            computerScore++
-        } else if (cInput == 'Paper' && pChoice == 'Rock') {
-            computerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Paper') {
-            playerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Paper') {
-            computerScore++;
-        } else if (cInput == 'Paper' && pChoice == 'Scissors') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Scissors') {
-            computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Lizard') {
-            playerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Lizard') {
-            computerScore++;
-        }  else if (cInput == 'Lizard' && pChoice == 'Scissors') {
-          computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Lizard') {
+           playerScore++;
+           computerScore++
+       } else if (cInput == 'Paper' && pChoice == 'Rock') {
+           computerScore++;
+       } else if (cInput == 'Rock' && pChoice == 'Paper') {
+           playerScore++;
+       } else if (cInput == 'Scissors' && pChoice == 'Paper') {
+           computerScore++;
+       } else if (cInput == 'Paper' && pChoice == 'Scissors') {
+           playerScore++;
+       } else if (cInput == 'Rock' && pChoice == 'Scissors') {
+           computerScore++;
+       } else if (cInput == 'Scissors' && pChoice == 'Rock') {
+           playerScore++;
+       } else if (cInput == 'Lizard' && pChoice == 'Spock') {
+           computerScore++;
+       } else if (cInput == 'Spock' && pChoice == 'Lizard') {
+           playerScore++;
+       } else if (cInput == 'Lizard' && pChoice == 'Rock') {
+           playerScore++;
+       } else if (cInput == 'Rock' && pChoice == 'Lizard') {
+           computerScore++;
+       }  else if (cInput == 'Lizard' && pChoice == 'Scissors') {
          computerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Paper') {
-            computerScore++;
+       } else if (cInput == 'Scissors' && pChoice == 'Lizard') {
+        computerScore++;
+       } else if (cInput == 'Lizard' && pChoice == 'Paper') {
+           computerScore++;
+       
+       } else if (cInput == 'Paper' && pChoice == 'Lizard') {
+           playerScore++;
+       } else if (cInput == 'Spock' && pChoice == 'Rock') {
+           playerScore++;
+       } else if (cInput == 'Rock' && pChoice == 'Spock') {
+           computerScore++;
+       } else if (cInput == 'Spock' && pChoice == 'Scissors') {
+           computerScore++;
+       } else if (cInput == 'Scissors' && pChoice == 'Spock') {
+           playerScore++;
+       } else if (cInput == 'Spock' && pChoice == 'Rock') {
+           computerScore++;
+       } else if (cInput == 'Rock' && pChoice == 'Spock') {
+           playerScore++;
+       } else if (cInput == 'Paper' && pChoice == 'Spock') {
+           computerScore++;
+       } else if (cInput == 'Spock' && pChoice == 'Paper') {
+           playerScore++;
+       }
         
-        } else if (cInput == 'Paper' && pChoice == 'Lizard') {
-            playerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Scissors') {
-            computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Spock') {
-            playerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Rock') {
-            computerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Spock') {
-            playerScore++;
-        } else if (cInput == 'Paper' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Paper') {
-            playerScore++;
-        }
-         
 
 
-        document.getElementById("player").innerHTML = playerScore;
+       document.getElementById("player").innerHTML = playerScore;
 
-        document.getElementById("computer").innerHTML = computerScore;
-
-document.getElementById("reset").addEventListener("click", function () {
-    document.getElementById("player").innerHTML = 0;
-   document.getElementById("computer").innerHTML = 0;
+       document.getElementById("computer").innerHTML = computerScore;
 
 
+document.getElementById("reset").addEventListener("click", function() {
+    resetCounter();
+})
+
+
+
+     });
+   });
    
-   
-} )
-      
-      });
-    });
-    
 
 
-function declareWinner() {
-    if (playerScore == 5) {
-        document.getElementById("result").innerHTML = "You win!";
-    } else if (computerScore == 5) {
-        document.getElementById("result").innerHTML = "You lose!";
-    }
+function resetCounter() {
+    playerScore = 0;
+    computerScore = 0;
+    updateDisplay(playerScore, computerScore);
 }
-
-    
     
 
-    // Increment by one for player
+   function updateDisplay(val) {
+       document.getElementById("player").innerHTML = val;
+       document.getElementById("computer").innerHTML = val;
+   }
+    
+
+
+    
+
+
+  
+   
 
 
 
+    
 
-// Increment by one for computer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
 
 
 
@@ -135,76 +122,18 @@ function declareWinner() {
 
 
 
-/*
-
-
-
-    function whoWins(pChoice, cInput) {
-
-      
-      
-        if (pChoice == cInput) {
-            playerScore++;
-            computerScore++
-        } else if (cInput == 'Paper' && pChoice == 'Rock') {
-            computerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Paper') {
-            playerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Paper') {
-            computerScore++;
-        } else if (cInput == 'Paper' && pChoice == 'Scissors') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Scissors') {
-            computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Lizard') {
-            playerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Lizard') {
-            computerScore++;
-        }  else if (cInput == 'Lizard' && pChoice == 'Scissors') {
-          computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Lizard') {
-         computerScore++;
-        } else if (cInput == 'Lizard' && pChoice == 'Paper') {
-            computerScore++;
-        
-        } else if (cInput == 'Paper' && pChoice == 'Lizard') {
-            playerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Rock') {
-            playerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Scissors') {
-            computerScore++;
-        } else if (cInput == 'Scissors' && pChoice == 'Spock') {
-            playerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Rock') {
-            computerScore++;
-        } else if (cInput == 'Rock' && pChoice == 'Spock') {
-            playerScore++;
-        } else if (cInput == 'Paper' && pChoice == 'Spock') {
-            computerScore++;
-        } else if (cInput == 'Spock' && pChoice == 'Paper') {
-            playerScore++;
-        }
-         
-
-
-    }
-
-whoWins();
-whoWins();
-whoWins();
-whoWins();
-
-console.log(computerScore);
 
 
 
 
-*/
+
+
+
+
+
+
+
+
+
+
+
